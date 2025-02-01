1e936ce3faf9cf0d2e6c6d108b6864c4f80e4378
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-01-31 17:27:51 -0800

    Update fib_bench.rs

--- benches/fib_bench.rs
+++ benches/fib_bench.rs
@@ -2,9 +2,7 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use onebfc::*;
 
 fn fib_bench(c: &mut Criterion) {
-    c.bench_function("fib 1_000_000", |b| b.iter(|| code::fib(1000000)))
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(100));
+    c.bench_function("fib 1_000_000", |b| b.iter(|| code::fib(1000000)));
 }
 
 criterion_group!(benches, fib_bench);
