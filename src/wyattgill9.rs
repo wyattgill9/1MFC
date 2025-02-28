3c654a0ac5a6d310b4f0e8782cfbd1410292bb72
Shidowy <101477459+Shidowy@users.noreply.github.com>
2025-02-28 15:59:59 -0800

    luc

--- src/wyattgill9.rs
+++ src/wyattgill9.rs
@@ -1,91 +1,51 @@
-use num_bigint::BigUint;
+use num_bigint::BigInt;
 
-#[derive(Clone)]
-struct FibPair {
-    a: BigUint,
-    b: BigUint,
+pub fn fib_luc(mut n: isize) -> (BigInt, BigInt) {
+    if n == 0 {
+        return (BigInt::ZERO, BigInt::from(2));
+    }
+
+    if n < 0 {
+        n = -n;
+        let (fib, luc) = fib_luc(n);
+        let k = if n % 2 == 0 { 1 } else { -1 };
+        return (fib * k, luc * k);
+    }
+
+    if n & 1 == 1 {
+        let (fib, luc) = fib_luc(n - 1);
+        return ((&fib + &luc) >> 1, (BigInt::from(5) * &fib + &luc) >> 1);
+    }
+
+    n >>= 1;
+    let k = (n % 2) * 2 - 1;
+    let (fib, luc) = fib_luc(n);
+    (&fib * &luc, &luc * &luc + BigInt::from(2) * k)
 }
 
-impl FibPair {
-    #[inline(always)]
-    fn new() -> Self {
-        FibPair {
-            a: BigUint::from(1u32),
-            b: BigUint::from(0u32),
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num_bigint::BigInt;
+
+    #[test]
+    fn test_base_cases() {
+        assert_eq!(fib_luc(0).0, BigInt::from(0));
+        assert_eq!(fib_luc(1).0, BigInt::from(1));
+        assert_eq!(fib_luc(2).0, BigInt::from(1));
     }
 
-    #[inline(always)]
-    fn square(&mut self) {
-        let a_squared = &self.a * &self.a;
-        let b_squared = &self.b * &self.b;
-        let mut double_ab = &self.a * &self.b;
-        double_ab <<= 1;
-
-        self.a = a_squared + &b_squared;
-        self.b = double_ab + &b_squared;
+    #[test]
+    fn test_small_values() {
+        assert_eq!(fib_luc(3).0, BigInt::from(2));
+        assert_eq!(fib_luc(4).0, BigInt::from(3));
+        assert_eq!(fib_luc(5).0, BigInt::from(5));
     }
-
-    #[inline(always)]
-    fn step(&mut self) {
-        self.a = std::mem::take(&mut self.b);
-        self.b += &self.a;
-    }
-}
-
-pub fn fib(n: usize) -> BigUint {
-    if n <= 1 {
-        return BigUint::from(n);
-    }
-
-    let mut pair = FibPair::new();
-    let bits = usize::BITS - n.leading_zeros();
 
     #[test]
     fn test_larger_values() {
         assert_eq!(fib_luc(10).0, BigInt::from(55));
         assert_eq!(fib_luc(15).0, BigInt::from(610));
         assert_eq!(fib_luc(20).0, BigInt::from(6765));
-    }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_fib() {
-        assert_eq!(fib(0), BigUint::from(0u32));
-        assert_eq!(fib(1), BigUint::from(1u32));
-        assert_eq!(fib(2), BigUint::from(1u32));
-        assert_eq!(fib(3), BigUint::from(2u32));
-        assert_eq!(fib(4), BigUint::from(3u32));
-        assert_eq!(fib(5), BigUint::from(5u32));
-        assert_eq!(fib(6), BigUint::from(8u32));
-        assert_eq!(fib(7), BigUint::from(13u32));
-        assert_eq!(fib(8), BigUint::from(21u32));
-        assert_eq!(fib(9), BigUint::from(34u32));
-        assert_eq!(fib(10), BigUint::from(55u32));
-        assert_eq!(fib(11), BigUint::from(89u32));
-        assert_eq!(fib(12), BigUint::from(144u32));
-        assert_eq!(fib(13), BigUint::from(233u32));
-        assert_eq!(fib(14), BigUint::from(377u32));
-        assert_eq!(fib(15), BigUint::from(610u32));
-        assert_eq!(fib(16), BigUint::from(987u32));
-        assert_eq!(fib(17), BigUint::from(1597u32));
-        assert_eq!(fib(18), BigUint::from(2584u32));
-        assert_eq!(fib(19), BigUint::from(4181u32));
-        assert_eq!(fib(20), BigUint::from(6765u32));
-        assert_eq!(fib(21), BigUint::from(10946u32));
-        assert_eq!(fib(22), BigUint::from(17711u32));
-        assert_eq!(fib(23), BigUint::from(28657u32));
-        assert_eq!(fib(24), BigUint::from(46368u32));
-        assert_eq!(fib(25), BigUint::from(75025u32));
-        assert_eq!(fib(26), BigUint::from(121393u32));
-        assert_eq!(fib(27), BigUint::from(196418u32));
-        assert_eq!(fib(28), BigUint::from(317811u32));
-        assert_eq!(fib(29), BigUint::from(514229u32));
-        assert_eq!(fib(30), BigUint::from(832040u32));
     }
 }
